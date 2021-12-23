015337bb611c1ff72f647207c06f41dab3a275d6
Sait <gamerpat70@gmail.com>
2021-12-23 13:22:14 +0300

    Commit 1 Sait

--- app/src/main/java/com/example/gamegal_app/Sign_in.kt
+++ app/src/main/java/com/example/gamegal_app/Sign_in.kt
@@ -3,7 +3,7 @@ package com.example.gamegal_app
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class Sign_in : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_in)
