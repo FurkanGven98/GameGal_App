414a6f61f7d1b84b3066f71b4c741c2e3050db3a
FurkanGven98 <53400436+FurkanGven98@users.noreply.github.com>
2022-01-27 13:53:16 +0300

    Commit 3 Furkan

--- app/src/main/java/com/example/gamegal_app/MainActivity.kt
+++ app/src/main/java/com/example/gamegal_app/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.gamegal_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.graphics.Typeface
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
