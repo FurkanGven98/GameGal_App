79ce096cbfb8582d0f260cd1b54abdb957c9e4ed
FurkanGven98 <53400436+FurkanGven98@users.noreply.github.com>
2022-02-17 23:05:02 +0300

    Commit 3 Furkan

--- app/src/main/java/com/example/gamegal_app/Fragments/ProfileFragment.kt
+++ app/src/main/java/com/example/gamegal_app/Fragments/ProfileFragment.kt
@@ -1,11 +1,14 @@
 package com.example.gamegal_app.Fragments
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.gamegal_app.AccountSettingsActivity
 import com.example.gamegal_app.R
+import kotlinx.android.synthetic.main.fragment_profile.view.*
 
 class ProfileFragment : Fragment() {
       override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,7 +20,13 @@ class ProfileFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+
+        val view = inflater.inflate(R.layout.fragment_profile, container, false)
+
+        view.edit_account_settings_btn.setOnClickListener {
+            startActivity(Intent(context,AccountSettingsActivity::class.java))
+        }
+        return view
     }
 
 }
