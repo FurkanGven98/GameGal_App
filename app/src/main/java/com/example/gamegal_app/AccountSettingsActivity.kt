18037b61ab3094465430385a09de962ebbb63bad
FurkanGven98 <53400436+FurkanGven98@users.noreply.github.com>
2022-02-19 15:48:40 +0300

    Commit 3 Furkan

--- app/src/main/java/com/example/gamegal_app/AccountSettingsActivity.kt
+++ app/src/main/java/com/example/gamegal_app/AccountSettingsActivity.kt
@@ -1,5 +1,6 @@
 package com.example.gamegal_app
 
+import android.content.Intent
 import android.os.Bundle
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
@@ -8,12 +9,24 @@ import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import com.example.gamegal_app.databinding.ActivityAccountSettingsBinding
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_account_settings.*
 
 class AccountSettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_account_settings)
+
+        logout_btn.setOnClickListener {
+            FirebaseAuth.getInstance().signOut()
+
+            val intent = Intent(this@AccountSettingsActivity, Sign_in::class.java)
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK)
+            startActivity(intent)
+            finish()
         }
+        }
+
     }
 
 
