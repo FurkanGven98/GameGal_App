3bc59366d6076153567acad544c83d9703ac63cc
FurkanGven98 <53400436+FurkanGven98@users.noreply.github.com>
2022-03-04 12:21:11 +0300

    Commit 5 Furkan

--- app/src/main/java/com/example/gamegal_app/Fragments/ProfileFragment.kt
+++ app/src/main/java/com/example/gamegal_app/Fragments/ProfileFragment.kt
@@ -1,5 +1,6 @@
 package com.example.gamegal_app.Fragments
 
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -7,10 +8,22 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.gamegal_app.AccountSettingsActivity
+import com.example.gamegal_app.Model.User
 import com.example.gamegal_app.R
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.fragment_profile.view.*
 
 class ProfileFragment : Fragment() {
+
+    private lateinit var profileId: String
+    private lateinit var firebaseUser: FirebaseUser
+
       override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
@@ -22,11 +35,138 @@ class ProfileFragment : Fragment() {
         // Inflate the layout for this fragment
 
         val view = inflater.inflate(R.layout.fragment_profile, container, false)
+        firebaseUser = FirebaseAuth.getInstance().currentUser!!
+        val pref = context?.getSharedPreferences("PREFS", Context.MODE_PRIVATE)
+
+        if(pref != null)
+        {
+            //sıkıntılı v12 dk 8
+            this.profileId = pref.getString("profileId","none").toString()
+        }
+        if(profileId == firebaseUser.uid)
+        {
+            view.edit_account_settings_btn.text = "Edit Profile"
+        }else if(profileId != firebaseUser.uid){
+            checkFollowandFollowingButtonStatus()
+        }
 
         view.edit_account_settings_btn.setOnClickListener {
             startActivity(Intent(context,AccountSettingsActivity::class.java))
         }
+        getFollowers()
+        getFollowings()
+        userInfo()
+
         return view
     }
+    private fun checkFollowandFollowingButtonStatus() {
+        val followingRef = firebaseUser?.uid.let { it1 ->
+            FirebaseDatabase.getInstance().reference
+                .child("Follow").child(it1.toString())
+                .child("Following")
+        }
+        if(followingRef != null){
+            followingRef.addValueEventListener(object: ValueEventListener {
+                override fun onDataChange(snapshot: DataSnapshot) {
+                    if(snapshot.child(profileId).exists()){
+                        view?.edit_account_settings_btn?.text = "Following"
+                    }else{
+                        view?.edit_account_settings_btn?.text = "Follow"
+                    }
+
+                }
+
+                override fun onCancelled(error: DatabaseError) {
+
+                }
+
+            })
+        }
+    }
+
+    private fun getFollowers(){
+        val followersRef =
+            FirebaseDatabase.getInstance().reference
+                .child("Follow").child(profileId)
+                .child("Followers")
+
+
+        followersRef.addValueEventListener(object :ValueEventListener{
+            override fun onDataChange(snapshot: DataSnapshot) {
+                if(snapshot.exists()){
+                    view?.total_followers?.text = snapshot.childrenCount.toString()
+                }
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+
+            }
+
+        })
+    }
+
+    private fun getFollowings(){
+        val followingsRef = FirebaseDatabase.getInstance().reference
+            .child("Follow").child(profileId)
+            .child("Following")
+
+
+        followingsRef.addValueEventListener(object :ValueEventListener{
+            override fun onDataChange(snapshot: DataSnapshot) {
+                if(snapshot.exists()){
+                    view?.total_following?.text = snapshot.childrenCount.toString()
+                }
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+
+            }
+
+        })
+    }
+
+    private fun userInfo(){
+        //YORUM
+        val usersRef = FirebaseDatabase.getInstance().reference.child("Users").child(profileId)
+        usersRef.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+
+                if(snapshot.exists())
+                {
+                    val user = snapshot.getValue<User>(User::class.java)
+                    Picasso.get().load(user!!.getImage()).placeholder(R.drawable.profile).into(view?.pro_image_profile_frag)
+                    view?.profile_fragment_username?.text = user!!.getUsername()
+                    view?.full_name_profile_frag?.text = user!!.getFullname()
+                    view?.bio_profile_frag?.text = user!!.getBio()
+                }
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+
+            }
+
+        })
+    }
+    override fun onStop() {
+        super.onStop()
+        val pref = context?.getSharedPreferences("PREFS",Context.MODE_PRIVATE)?.edit()
+        pref?.putString("profileId",firebaseUser.uid)
+        pref?.apply()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        val pref = context?.getSharedPreferences("PREFS",Context.MODE_PRIVATE)?.edit()
+        pref?.putString("profileId",firebaseUser.uid)
+        pref?.apply()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        val pref = context?.getSharedPreferences("PREFS",Context.MODE_PRIVATE)?.edit()
+        pref?.putString("profileId",firebaseUser.uid)
+        pref?.apply()
+    }
+
 
 }
