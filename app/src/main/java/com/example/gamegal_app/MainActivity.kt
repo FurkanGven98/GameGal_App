a19ded85e39042e0267fa93b66de4bdfd1d85ebe
FurkanGven98 <53400436+FurkanGven98@users.noreply.github.com>
2022-04-15 14:10:36 +0300

    Commit 9 Furkan

--- app/src/main/java/com/example/gamegal_app/MainActivity.kt
+++ app/src/main/java/com/example/gamegal_app/MainActivity.kt
@@ -1,13 +1,21 @@
 package com.example.gamegal_app
 
+import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.view.animation.AnimationUtils
 import androidx.fragment.app.Fragment
 import com.example.gamegal_app.Fragments.*
+import com.example.gamegal_app.databinding.ActivityMainBinding
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import kotlinx.android.synthetic.main.activity_main.*
 
+@Suppress("DEPRECATION")
 class MainActivity : AppCompatActivity() {
+    private val splashScreen = 10000
+    lateinit var binding: ActivityMainBinding
 
 
     private val onNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
@@ -45,6 +53,20 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val animasyon1 = AnimationUtils.loadAnimation(this,R.anim.animasyon1)
+        val animasyon2 = AnimationUtils.loadAnimation(this,R.anim.animasyon2)
+
+        val imageView=binding.imageView3
+        val imageView2=binding.imageView4
+        imageView3.animation = animasyon1
+        imageView4.animation = animasyon2
+
+        //SplashScreen
+        Handler().postDelayed({
+        finish()
+        },splashScreen.toLong())
 
         val navView: BottomNavigationView = findViewById(R.id.nav_view)
 
