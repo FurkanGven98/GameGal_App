2a9fff75462320fba7713e927142d86efc93a570
Sait <gamerpat70@gmail.com>
2022-04-17 15:33:50 +0300

    commit Sait

--- app/src/main/java/com/example/gamegal_app/MainActivity.kt
+++ app/src/main/java/com/example/gamegal_app/MainActivity.kt
@@ -5,17 +5,34 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.util.Log
+import android.util.Log.e
+import android.view.View
 import android.view.animation.AnimationUtils
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.example.gamegal_app.Fragments.*
 import com.example.gamegal_app.databinding.ActivityMainBinding
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.tasks.Task
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.firebase.FirebaseApp
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
+
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_sign_in.*
 
 @Suppress("DEPRECATION")
 class MainActivity : AppCompatActivity() {
     private val splashScreen = 10000
     lateinit var binding: ActivityMainBinding
+
+
 
 
     private val onNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
@@ -53,6 +70,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        /*
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         val animasyon1 = AnimationUtils.loadAnimation(this,R.anim.animasyon1)
@@ -67,6 +85,21 @@ class MainActivity : AppCompatActivity() {
         Handler().postDelayed({
         finish()
         },splashScreen.toLong())
+*/
+/*
+        FirebaseApp.initializeApp(this)
+
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso)
+        firebaseAuth = FirebaseAuth.getInstance()
+*/
+
+
+
 
         val navView: BottomNavigationView = findViewById(R.id.nav_view)
 
@@ -74,6 +107,14 @@ class MainActivity : AppCompatActivity() {
 
         moveToFragment(HomeFragment())
     }
+
+
+
+
+
+
+
+
     private fun moveToFragment(fragment: Fragment){
         val fragmentTrans=supportFragmentManager.beginTransaction()
         fragmentTrans.replace(R.id.fragment_container,fragment)
--- app/src/main/java/com/example/gamegal_app/Sign_in.kt
+++ app/src/main/java/com/example/gamegal_app/Sign_in.kt
@@ -6,11 +6,25 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.TextUtils
 import android.widget.Toast
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.tasks.Task
+import com.google.firebase.FirebaseApp
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
 import kotlinx.android.synthetic.main.activity_sign_in.*
 import kotlinx.android.synthetic.main.activity_sign_up.*
 
 class Sign_in : AppCompatActivity() {
+
+
+    lateinit var mGoogleSignInClient: GoogleSignInClient
+    val Req_Code: Int = 123
+    private lateinit var firebaseAuth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_in)
@@ -22,6 +36,22 @@ class Sign_in : AppCompatActivity() {
         login_btn.setOnClickListener {
             loginUser()
         }
+
+        FirebaseApp.initializeApp(this)
+
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso)
+        firebaseAuth = FirebaseAuth.getInstance()
+
+        Signin.setOnClickListener {
+            Toast.makeText(this, "Logging In", Toast.LENGTH_SHORT).show()
+            signInGoogle()
+        }
+
     }
     private fun loginUser() {
 
@@ -59,6 +89,16 @@ class Sign_in : AppCompatActivity() {
 
     override fun onStart() {
         super.onStart()
+
+        if (GoogleSignIn.getLastSignedInAccount(this) != null) {
+            startActivity(
+                Intent(
+                    this, MainActivity
+                    ::class.java
+                )
+            )
+            finish()
+        }
         if(FirebaseAuth.getInstance().currentUser !=null){
             val intent = Intent(this@Sign_in,MainActivity::class.java)
             intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK)
@@ -66,4 +106,52 @@ class Sign_in : AppCompatActivity() {
             finish()
         }
     }
+
+
+
+
+    private fun signInGoogle() {
+
+        val signInIntent: Intent = mGoogleSignInClient.signInIntent
+        startActivityForResult(signInIntent, Req_Code)
+    }
+
+
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == Req_Code) {
+            val task: Task<GoogleSignInAccount> = GoogleSignIn.getSignedInAccountFromIntent(data)
+            handleResult(task)
+        }
+    }
+
+    private fun handleResult(completedTask: Task<GoogleSignInAccount>) {
+        try {
+            val account: GoogleSignInAccount? = completedTask.getResult(ApiException::class.java)
+            if (account != null) {
+                UpdateUI(account)
+            }
+        } catch (e: ApiException) {
+            Toast.makeText(this, e.toString(), Toast.LENGTH_SHORT).show()
+        }
+    }
+
+
+    private fun UpdateUI(account: GoogleSignInAccount) {
+        val credential = GoogleAuthProvider.getCredential(account.idToken, null)
+        firebaseAuth.signInWithCredential(credential).addOnCompleteListener { task ->
+            if (task.isSuccessful) {
+                Toast.makeText(this, account.email.toString().toString(), Toast.LENGTH_LONG).show()
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+                finish()
+
+
+            }
+        }
+    }
+
+
+
 }
